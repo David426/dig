407fdab82dd3b37edee974ced3963b8af10d09b8
David Sill <david.sill@stonybrook.edu>
2019-03-05 15:16:14 -0500

    Loop to fully explore and return the IP of a domain

--- src/MyDig.java
+++ src/MyDig.java
@@ -8,32 +8,57 @@ import java.net.UnknownHostException;
 
 
 public class MyDig {
-    static int ANSWER_SECTION = 1;
-    static int AUTHORITY_RECORDS_SECTON = 2;
-    static int ADDITIONAL_RECORDS_SECTION = 3;
 
     static String[] ROOT_SERVERS = {"198.41.0.4", "199.9.14.201", "192.33.4.12", "199.7.91.13", "192.203.230.10", "192.5.5.241",
     "192.112.36.4", "198.97.190.53", "192.36.148.17", "192.58.128.30", "193.0.14.129", "199.7.83.42", "202.12.27.33"};
 
     public static void main(String[] args){
         Boolean isResolved = false;
-        Message queryMessage = createQuery(args[0]);
+        String queryAddress = args[0];
+        Message queryMessage = createQuery(queryAddress);
 //        System.out.println(queryMessage.toString());
 
         try {
             SimpleResolver resolver = new SimpleResolver(ROOT_SERVERS[0]);
-            Message reply = resolver.send(queryMessage);
-            System.out.println(reply.toString());
-            Record[] answers = reply.getSectionArray(ANSWER_SECTION);
-            Record[] authorityRecords = reply.getSectionArray(AUTHORITY_RECORDS_SECTON);
-            Record[] additionalRecords = reply.getSectionArray(ADDITIONAL_RECORDS_SECTION);
-            System.out.println(reply.findRecord(authorityRecords[0], 2));
-            for(int i = 0; i < additionalRecords.length; i++){
-                System.out.println(additionalRecords[i].getType());
-                if(additionalRecords[i].getType() == Type.A){
-                    System.out.println(((ARecord)additionalRecords[i]).getAddress());
+            while (!isResolved) {
+                Message reply = resolver.send(queryMessage);
+                System.out.println(reply.toString());
+                Record[] answers = reply.getSectionArray(Section.ANSWER);
+                Record[] authorityRecords = reply.getSectionArray(Section.AUTHORITY);
+                Record[] additionalRecords = reply.getSectionArray(Section.ADDITIONAL);
+//                System.out.println(reply.findRecord(authorityRecords[0], 2));
+
+                if(answers.length > 0){
+                    System.out.println("###### Answer Found");
+                    if (answers[0].getType() == Type.CNAME) {
+                        System.out.printf("CNAME: %s\n", ((CNAMERecord)answers[0]).getTarget().toString());
+                        queryAddress = ((CNAMERecord)answers[0]).getTarget().toString();
+                        resolver = new SimpleResolver(ROOT_SERVERS[0]);
+//                        isResolved=  true;
+                    } else {
+                        System.out.println("##### QUERY RESOLVED!!#####");
+                        System.out.println(reply);
+                        System.out.println(answers[0]);
+                        isResolved = true;
+                    }
+                } else if(additionalRecords.length > 0){
+                    for (int i = 0; i < additionalRecords.length; i++) {
+                        if (additionalRecords[i].getType() == Type.A) {
+//                            queryAddress = ((ARecord) additionalRecords[i]).getAddress().getHostAddress();
+                            resolver = new SimpleResolver(((ARecord) additionalRecords[i]).getAddress().getHostAddress());
+                            break;
+                        }
+                    }
+                } else if (authorityRecords.length > 0) {
+//                    queryAddress = authorityRecords[0].getName()
+                    System.out.println("#####AUTH RECORD NAME######");
+                    queryAddress = ((SOARecord)authorityRecords[0]).getAdmin().toString();
+                } else {
+                    System.out.println("Something went wrong :/");
+                    System.out.println(reply);
+                    isResolved = true;
                 }
-//                resolver = new SimpleResolver(additionalRecords[0].)
+                queryMessage = createQuery(queryAddress);
             }
 
         } catch (UnknownHostException e) {
@@ -41,27 +66,6 @@ public class MyDig {
         } catch (IOException e){
             e.printStackTrace();
         }
-//            SimpleResolver resolver = new SimpleResolver("205.251.192.15");
-////151.101.210.49
-//            Message replyMessage = resolver.send(queryMessage);
-//            System.out.println(replyMessage.toString());
-
-
-
-        //Our goal
-//        try{
-//            Lookup look = new Lookup(new Name(args[0]));
-//              Lookup look = new Lookup(new Name("www.news12.com"));
-//
-//            look.run();
-//            Record[] records = look.getAnswers();
-//            for (Record rec:records) {
-//                System.out.println(rec.toString());
-//            }
-//        }
-//        catch (Exception e){
-//            System.out.println(e.toString());
-//        }
 
     }
 
